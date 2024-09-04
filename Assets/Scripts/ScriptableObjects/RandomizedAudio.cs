ba0a8505a5f169e4e49f090662fd636f4687eb02
PierceNunn <agentp5151@gmail.com>
2024-09-04 09:22:18 -0500

    imported voice samples

--- Assets/Scripts/ScriptableObjects/RandomizedAudio.cs
+++ Assets/Scripts/ScriptableObjects/RandomizedAudio.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class RandomizedAudio : MonoBehaviour
+[CreateAssetMenu]
+public class RandomizedAudio : ScriptableObject
 {
     
     public AudioClip[] clips;
-    public string teste;
 }
