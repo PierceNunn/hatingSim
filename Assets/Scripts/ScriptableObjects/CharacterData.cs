43ef714dec9e6b5a197125c00db4e24e28221496
PierceNunn <agentp5151@gmail.com>
2024-09-04 09:40:14 -0500

    integrated CharacterData into Dialogue script

--- Assets/Scripts/ScriptableObjects/CharacterData.cs
+++ Assets/Scripts/ScriptableObjects/CharacterData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu]
 public class CharacterData : ScriptableObject
 {
+    [SerializeField] private string _characterName;
     [SerializeField] private Sprite _characterPortrait;
     [SerializeField] private RandomizedAudio _characterVoice;
 }
--- Assets/Scripts/SerializableObjects/SingleDialogue.cs
+++ Assets/Scripts/SerializableObjects/SingleDialogue.cs
@@ -6,18 +6,11 @@ using UnityEngine.Events;
 [System.Serializable]
 public class SingleDialogue
 {
-    [SerializeField] private string _name;
-    [SerializeField] private Sprite _talkerIMG;
-    [SerializeField] private RandomizedAudio _voice;
-    [SerializeField] private bool _jitterText = false;
+    [SerializeField] private CharacterData _talkerData;
     [SerializeField] private UnityEvent[] _eventsToInvoke; //for calling events mid-conversation
     [TextArea(3, 10)]
     [SerializeField] private string _sentences;
 
-    public string name { get => _name; set => _name = value; }
-    public Sprite talkerIMG { get => _talkerIMG; set => _talkerIMG = value; }
-    public RandomizedAudio voice { get => _voice; set => _voice = value; }
-    public bool jitterText { get => _jitterText; set => _jitterText = value; }
     public UnityEvent[] EventsToInvoke { get => _eventsToInvoke; set => _eventsToInvoke = value; }
     public string sentences { get => _sentences; set => _sentences = value; }
 }
