f953ee7b6890918b7d5ac623dcd6d88c0bbda13c
PierceNunn <agentp5151@gmail.com>
2024-09-09 10:46:19 -0500

    building DialogueNode system (WIP)

--- Assets/Scripts/Nodes/DialogueNode.cs
+++ Assets/Scripts/Nodes/DialogueNode.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 using XNode;
 
 public class DialogueNode : Node {
-
+	[Output] [SerializeField] private Node node;
+	[Input] [SerializeField] private string _responseName;
 	[SerializeField] private SingleDialogue[] _dialogue;
-	[SerializeField] private DialogueResponse[] _responses;
+	[Output(dynamicPortList = true)] [SerializeField] private Node[] _responses;
 
-	public SingleDialogue[] Dialogue { get => _dialogue; set => _dialogue = value; }
-	public DialogueResponse[] Responses { get => _responses; set => _responses = value; }
 
-	// Use this for initialization
-	protected override void Init() {
+    // Use this for initialization
+    protected override void Init() {
 		base.Init();
 		
 	}
 
 	// Return the correct value of an output port when requested
 	public override object GetValue(NodePort port) {
+		if (port.fieldName == "node") 
+			return GetInputValue<Node>("node", this);
 		return null; // Replace this
 	}
 }
