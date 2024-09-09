a64bbe87c9feea1c2c1cf1f24237cb32d206ed21
PierceNunn <agentp5151@gmail.com>
2024-09-09 11:45:50 -0500

    separated dialogue choices into separate node

--- Assets/Scripts/Nodes/DialogueBranchNode.cs
+++ Assets/Scripts/Nodes/DialogueBranchNode.cs
@@ -3,22 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
-public class DialogueNode : Node {
+public class DialogueBranchNode : Node {
 	[Output] [SerializeField] private Node _lastNode;
-	[SerializeField] private SingleDialogue _dialogue;
-	[Input] [SerializeField] private Node _nextNode;
-	
-
-
-    // Use this for initialization
-    protected override void Init() {
+	[Input(dynamicPortList = true)] [SerializeField] private Node[] _responses;
+	// Use this for initialization
+	protected override void Init() {
 		base.Init();
 		
 	}
 
 	// Return the correct value of an output port when requested
 	public override object GetValue(NodePort port) {
-		if (port.fieldName == "_lastNode") 
+		if (port.fieldName == "_lastNode")
 			return GetInputValue<Node>("_lastNode", this);
 		return null; // Replace this
 	}
