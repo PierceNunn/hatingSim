1e9d8e05601dfb9310eebebf39c352668ca11cf5
PierceNunn <agentp5151@gmail.com>
2024-09-09 12:34:32 -0500

    DialogueBranchNode branches now readable

--- Assets/Scripts/Nodes/DialogueBranchNode.cs
+++ Assets/Scripts/Nodes/DialogueBranchNode.cs
@@ -6,10 +6,20 @@ using XNode;
 public class DialogueBranchNode : Node {
 	[Output] [SerializeField] private Node _lastNode;
 	[Input(dynamicPortList = true)] [SerializeField] private Node[] _responses;
+
+	public Node[] nextNodes;
 	// Use this for initialization
 	protected override void Init() {
 		base.Init();
-		
+		nextNodes = new Node[_responses.Length];
+		for(int i = 0; i < _responses.Length; i++)
+        {
+			NodePort nextPort = GetInputPort("_responses " + i).Connection;
+			if (nextPort != null)
+			{
+				nextNodes[i] = nextPort.node as Node;
+			}
+		}
 	}
 
 	// Return the correct value of an output port when requested
