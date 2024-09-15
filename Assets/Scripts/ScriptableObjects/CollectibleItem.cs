b5de087084e300b588040bb6066f12408ddbf4aa
PierceNunn <agentp5151@gmail.com>
2024-09-15 15:42:05 -0500

    Created ScriptableObject CollectibleItem

---
+++ Assets/Scripts/ScriptableObjects/CollectibleItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CollectibleItem : ScriptableObject
+{
+    [SerializeField] private string _itemID;
+    [SerializeField] private Sprite _itemImage;
+}
