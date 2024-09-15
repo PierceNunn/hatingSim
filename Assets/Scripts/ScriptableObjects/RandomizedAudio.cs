15b7df746dc716d8535edd6da99a1cbf69013bff
PierceNunn <agentp5151@gmail.com>
2024-09-15 17:02:40 -0500

    Commented on RandomizedAudio

--- Assets/Scripts/ScriptableObjects/RandomizedAudio.cs
+++ Assets/Scripts/ScriptableObjects/RandomizedAudio.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * RandomizedAudio's a very simple class basically for easily storing
+ * multiple audio clips in a place where they can be easily reused
+ * (mainly meant for banjo-kazooie type voices)
+ */
 [CreateAssetMenu]
 public class RandomizedAudio : ScriptableObject
 {
