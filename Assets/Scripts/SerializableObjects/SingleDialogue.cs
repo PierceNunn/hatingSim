7a25b650c4c84f2e99cf9c5cfc7d1d360091bc5f
PierceNunn <agentp5151@gmail.com>
2024-09-15 17:20:01 -0500

    added comments to SingleDialogue

--- Assets/Scripts/SerializableObjects/SingleDialogue.cs
+++ Assets/Scripts/SerializableObjects/SingleDialogue.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+/*
+ * SingleDialogue is the main form of storage for chunks of dialogue, holding
+ * the speaking character, events to call when displayed, and of course the
+ * dialogue text itself.
+ */
 [System.Serializable]
 public class SingleDialogue
 {
     [SerializeField] private CharacterData _talkerData;
     [SerializeField] private UnityEvent[] _eventsToInvoke; //for calling events mid-conversation
-    [TextArea(3, 10)]
+    [TextArea(3, 10)] //set size of text field in editor
     [SerializeField] private string _sentences;
 
     public UnityEvent[] EventsToInvoke { get => _eventsToInvoke; set => _eventsToInvoke = value; }
