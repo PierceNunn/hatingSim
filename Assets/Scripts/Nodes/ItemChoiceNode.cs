bd95f94851c8585e7f702a8d7dfe0aefd3ac4ae9
PierceNunn <agentp5151@gmail.com>
2024-09-15 16:36:33 -0500

    Commented on ItemChoiceNode

--- Assets/Scripts/Nodes/ItemChoiceNode.cs
+++ Assets/Scripts/Nodes/ItemChoiceNode.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
+/*
+ * ItemChoiceNode inherits from ChoiceNode and adds parameter for required item
+ * if the required item is not flagged as obtained in save data, the choice
+ * won't be accessible (most of which is processed in DialogueManager)
+ */
 public class ItemChoiceNode : ChoiceNode
 {
     [SerializeField] private string _requiredItem;
