1d088ed6cb013d0df6fd0cf287c4b06248c88cbc
PierceNunn <agentp5151@gmail.com>
2024-09-15 16:29:43 -0500

    added comments to DebugFunctions

--- Assets/Scripts/Debug/DebugFunctions.cs
+++ Assets/Scripts/Debug/DebugFunctions.cs
@@ -5,29 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class DebugFunctions : MonoBehaviour
 {
-
+    /// <summary>
+    /// Sets all CollectibleItem objects as not found.
+    /// </summary>
     public void ClearItemData()
     {
+        //finds all CollectibleItems in Resources folder
         CollectibleItem[] itemsToClear = 
         Resources.LoadAll<CollectibleItem>("Items");
 
         foreach(CollectibleItem c in itemsToClear)
         {
+            //using 0 as false and 1 as true as PP doesn't support bools
             PlayerPrefs.SetInt(c.ItemID, 0);
         }
     }
+    /// <summary>
+    /// Sets all CollectibleItem objects as found.
+    /// </summary>
     public void GainAllItems()
     {
+        //finds all CollectibleItems in Resources folder
         CollectibleItem[] itemsToGain =
         Resources.LoadAll<CollectibleItem>("Items");
 
         foreach (CollectibleItem c in itemsToGain)
         {
+            //using 0 as false and 1 as true as PP doesn't support bools
             PlayerPrefs.SetInt(c.ItemID, 1);
         }
     }
 
-
+    /// <summary>
+    /// Reloads the currently loaded scene.
+    /// </summary>
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
