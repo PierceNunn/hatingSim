30438b6363ed738434941d926ae2f64a070bce34
PierceNunn <agentp5151@gmail.com>
2024-09-15 16:55:22 -0500

    added comments to DialogueGiver

--- Assets/Scripts/UI/DialogueGiver.cs
+++ Assets/Scripts/UI/DialogueGiver.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
+/*
+ * DialogueGiver is where dialogue is actually stored
+ * the InitiateDialogue function also needs to be called to push the stored
+ * dialogue to the text box/UI
+ */
 public class DialogueGiver : MonoBehaviour
 {
     [SerializeField] private LinkedNode _dialogueToGive;
     [SerializeField] private bool _giveDialogueOnStart;
 
-    public LinkedNode DialogueToGive { get => _dialogueToGive; set => _dialogueToGive = value; }
+    public LinkedNode DialogueToGive { get => _dialogueToGive; 
+        set => _dialogueToGive = value; }
 
     private void Start()
     {
@@ -19,10 +25,12 @@ public class DialogueGiver : MonoBehaviour
     }
     public void InitiateDialogue()
     {
+        //if there's a graph on the object get the Intro Node from there
         if(GetComponent<DialogueSceneGraph>() != null)
         {
             _dialogueToGive = GetComponent<DialogueSceneGraph>().graph.findIntroNode();
         }
+        //send the stored Intro Node to the DialogueManager
         FindObjectOfType<DialogueManager>().StartDialogue(DialogueToGive, gameObject, false);
     }
 }
