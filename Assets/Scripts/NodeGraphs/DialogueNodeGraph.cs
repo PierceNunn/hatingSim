183646c129ddac9821592892542a2eef342c63cd
PierceNunn <agentp5151@gmail.com>
2024-09-15 16:26:58 -0500

    wrote description of findIntroNode

--- Assets/Scripts/NodeGraphs/DialogueNodeGraph.cs
+++ Assets/Scripts/NodeGraphs/DialogueNodeGraph.cs
@@ -6,11 +6,15 @@ using XNode;
 [CreateAssetMenu]
 public class DialogueNodeGraph : NodeGraph
 {
+    /// <summary>
+    /// Iterates through NodeGraph to find a node of type IntroNode.
+    /// Used to find the intro point for dialogue.
+    /// </summary>
+    /// <returns>Entry point of DialogueNodeGraph.</returns>
     public IntroNode findIntroNode()
     {
         for(int i = 0; i < nodes.Count; i++)
         {
-            Debug.Log(nodes[i].GetType());
             if(nodes[i].GetType().ToString().Equals("IntroNode"))
                 return nodes[i] as IntroNode;
         }
