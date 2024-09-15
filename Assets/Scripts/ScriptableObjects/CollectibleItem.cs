533e255d41f3b38dcbe5f9d39399d0e47864e7f1
PierceNunn <agentp5151@gmail.com>
2024-09-15 17:00:38 -0500

    commented in CollectibleItem

--- Assets/Scripts/ScriptableObjects/CollectibleItem.cs
+++ Assets/Scripts/ScriptableObjects/CollectibleItem.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [CreateAssetMenu]
 public class CollectibleItem : ScriptableObject
 {
-    [SerializeField] private string _itemID;
-    [SerializeField] private Sprite _itemImage;
+    [SerializeField] private string _itemID; //ID to store data under in PlayerPrefs
+    [SerializeField] private Sprite _itemImage; //image to represent item
 
     public string ItemID { get => _itemID; set => _itemID = value; }
     public Sprite ItemImage { get => _itemImage; set => _itemImage = value; }
