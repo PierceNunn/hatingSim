e08ce61db5d12c7d9c876670ea7e03667ddb8bc0
PierceNunn <agentp5151@gmail.com>
2024-09-15 16:34:24 -0500

    added comments to LinkedNode

--- Assets/Scripts/Nodes/LinkedNode.cs
+++ Assets/Scripts/Nodes/LinkedNode.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
+/* 
+ * LinkedNode isn't meant to be used directly
+ * instead, it builds on the existing Node class with references to a
+ * next and previous node, and then is inherited by most dialogue-related
+ * nodes
+ * This reduces code reuse and allows pulling of next/last node without knowing
+ * which type of node any given node is
+ */
 public class LinkedNode : Node {
 	[Output] [SerializeField] private Node _lastNode;
 	[Input] [SerializeField] private Node _nextNode;
@@ -15,8 +23,9 @@ public class LinkedNode : Node {
     // Use this for initialization
     protected override void Init() {
 		base.Init();
+		//Gets the nextNode port
 		NodePort nextPort = GetInputPort("_nextNode").Connection;
-		if (nextPort != null)
+		if (nextPort != null) //sets nextNode as nextPort's connection
 		{
 			NextNode = nextPort.node as Node;
 		}
