4e2320f6b96963e296ecf42a5d3a54518513b715
MarieChillOut <159060706+MarieChillOut@users.noreply.github.com>
2024-09-16 16:49:16 -0500

    Final Player Control Commit

--- Assets/Scripts/PlayerController/NPC/InteractController.cs
+++ Assets/Scripts/PlayerController/NPC/InteractController.cs
@@ -3,31 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NPC_Controller : MonoBehaviour
+public class InteractController : MonoBehaviour
 {
-    private bool isTriggered;
-
     private PlayerMovement pM;
 
     void Start()
     {
         pM = FindObjectOfType<PlayerMovement>();
-        isTriggered = false;
-
-
     }
 
     //When entering a hitbox & pressing space, interaction event should be able to happen
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        pM.InteractTextChange(true);
-        isTriggered = true;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            pM.InteractTextChange(true);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         pM.InteractTextChange(false);
-        isTriggered = false;
     }
 
     void Update()
--- Assets/Scripts/PlayerController/PlayerMovement.cs
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -7,62 +7,51 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private TMP_Text interactText;
+
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject cam;
 
     [SerializeField] private PlayerInput playerInput;
+
 
     private Rigidbody2D rb;
 
     private Vector2 direction;
 
-    private int speed;
+    [SerializeField] private int speed;
 
     private InputAction move;
     private InputAction select;
-
-    private bool isMoving;
 
     void Start()
     {
         rb = player.GetComponent<Rigidbody2D>();
         interactText.gameObject.SetActive(false);
 
-        playerInput.currentActionMap.Enable();
-        playerInput.actions.FindActionMap("Player");
-        move = playerInput.currentActionMap.FindAction("Move");
-        select = playerInput.currentActionMap.FindAction("Select");
+        move = playerInput.actions["Move"];
+        select = playerInput.actions["Select"];
 
-        speed = 2;
-
-        isMoving = false; 
-
-        move.started += Move_started;
-        move.canceled += Move_Canceled;
+        select.started += Select_started;
     }
 
-    private void Move_started(InputAction.CallbackContext obj)
+    private void Select_started(InputAction.CallbackContext obj)
     {
-        isMoving = true;
-        direction = obj.ReadValue<Vector2>();
+
     }
 
-    private void Move_Canceled(InputAction.CallbackContext obj)
-    {
-        isMoving = false;
-    }
-
+    //changes the [Space] text when in an item's hit box
     public void InteractTextChange(bool x)
     {
         interactText.gameObject.SetActive(x);
     }
 
-    
+
 
     void Update()
     {
-        if (isMoving)
-        {
-            rb.velocity = direction * speed;
-        }
+        direction = move.ReadValue<Vector2>();
+        rb.velocity = direction * speed;
+
+        cam.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
     }
 }
