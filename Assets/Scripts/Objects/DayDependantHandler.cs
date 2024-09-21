7e8ca9b0b86ad9093974c9cae02ac7283a1796fe
kittynomad <59345740+kittynomad@users.noreply.github.com>
2024-09-21 16:37:46 -0400

    implemented DayDependantHandler

--- Assets/Scripts/Objects/DayDependantHandler.cs
+++ Assets/Scripts/Objects/DayDependantHandler.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class DayDependantHandler : MonoBehaviour
 {
-    
-
     [SerializeField] private int _availableDay;
     [SerializeField] private TimeUIManager.DayPhases _availableTime;
+    [SerializeField] private GameObject _timeDependantEntity;
+
+    private void Update()
+    {
+        CheckIfAvailableNow();
+    }
+
+    void CheckIfAvailableNow()
+    {
+        if (PlayerPrefs.GetInt("currentDay", 0) == _availableDay && PlayerPrefs.GetInt("currentTime", 0) == (int)_availableTime)
+            _timeDependantEntity.SetActive(true);
+        else
+            _timeDependantEntity.SetActive(false);
+    }
 }
