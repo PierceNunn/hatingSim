b68ad1f205f99f195126c29ae337e79087ed6aec
kittynomad <59345740+kittynomad@users.noreply.github.com>
2024-09-21 16:27:54 -0400

    implemented AdvanceTime

--- Assets/Scripts/UI/TimeUIManager.cs
+++ Assets/Scripts/UI/TimeUIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,8 +24,25 @@ public class TimeUIManager : MonoBehaviour
         UpdateTimeUI();
     }
 
-    void Update()
+    private void Update()
     {
+        UpdateTimeUI();
+    }
+
+    public static void AdvanceTime()
+    {
+        if((int)currentTime + 1 > Enum.GetNames(typeof(DayPhases)).Length)
+        {
+            currentTime = 0;
+            currentDay += 1;
+        }
+        else
+        {
+            currentTime += 1;
+        }
+
+        PlayerPrefs.SetInt("currentDay", currentDay);
+        PlayerPrefs.SetInt("currentTime", (int)currentTime);
         
     }
 
