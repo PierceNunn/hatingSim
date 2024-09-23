4f0afe17e7cd0d1e1a57ff16c373f5e9131ac3b3
MarieChillOut <159060706+MarieChillOut@users.noreply.github.com>
2024-09-23 10:38:22 -0500

    New Script and TilemapAreas

---
+++ Assets/Scripts/Map Scripts/New Map Scripts/TileMapTeleport.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TileMapTeleport : MonoBehaviour
+{
+
+    [SerializeField] private Vector2[] newPos;
+    [SerializeField] private int toArea;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
