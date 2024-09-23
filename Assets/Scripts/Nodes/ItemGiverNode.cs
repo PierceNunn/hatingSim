e37ea160fc2c85993dedde0b8beedb64634f9217
kittynomad <59345740+kittynomad@users.noreply.github.com>
2024-09-23 05:09:13 -0500

    created ItemGiverNode

---
+++ Assets/Scripts/Nodes/ItemGiverNode.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGiverNode : DialogueNode
+{
+    [SerializeField] private CollectibleItem _itemToGive;
+}
