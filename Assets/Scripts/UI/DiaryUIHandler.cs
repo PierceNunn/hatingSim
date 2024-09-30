c0cb2c6e43ffd55051da4152015176bf261043a1
PierceNunn <agentp5151@gmail.com>
2024-09-30 09:48:58 -0500

    implemented UpdateDiaryDisplay

--- Assets/Scripts/UI/DiaryUIHandler.cs
+++ Assets/Scripts/UI/DiaryUIHandler.cs
@@ -11,8 +11,30 @@ public class DiaryUIHandler : MonoBehaviour
     [SerializeField] private Image _characterImage;
     [SerializeField] private Image[] _evidenceImages;
 
+    private int charIndex = 0;
+
     private void UpdateDiaryDisplay()
     {
+        CharacterData currentChar;
 
+        try
+        {
+            currentChar = _displayedCharacters[charIndex];
+        }
+        catch
+        {
+            print("invalid character reference in DiaryUIHandler!!");
+            return;
+        }
+
+        _characterName.text = currentChar.CharacterName;
+        _characterImage.sprite = currentChar.DefaultCharacterPortrait;
+
+        UpdateEvidenceDisplay();
+    }
+
+    private void UpdateEvidenceDisplay()
+    {
+        //update displayed evidence
     }
 }
