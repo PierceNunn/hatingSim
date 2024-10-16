af315da290a3997209246c050e9cbe26c553d858
PierceNunn <agentp5151@gmail.com>
2024-10-16 09:48:09 -0500

    implemented ItemQuantityChoiceNode functionality

--- Assets/Scripts/Nodes/ItemQuantityChoiceNode.cs
+++ Assets/Scripts/Nodes/ItemQuantityChoiceNode.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
-public class ItemQuantityChoiceNode : Node {
+public class ItemQuantityChoiceNode : ItemChoiceNode {
 
-	// Use this for initialization
-	protected override void Init() {
-		base.Init();
-		
-	}
-
-	// Return the correct value of an output port when requested
-	public override object GetValue(NodePort port) {
-		return null; // Replace this
-	}
+    [SerializeField] private int _quantityNeeded;
+    public override bool IsSelectable()
+    {
+        int quantityOwned = 0;
+        foreach(CollectibleItem c in RequiredItem)
+        {
+            if (CollectibleItem.IsItemCollected(c))
+                quantityOwned++;
+        }
+        if (quantityOwned >= _quantityNeeded)
+            return true;
+        return false;
+    }
 }
