d047a4f2fb66e1f2e092a88e854733a149b2ef4d
kittynomad <59345740+kittynomad@users.noreply.github.com>
2024-10-22 16:19:07 -0400

    implemented ReturnToMainMenu to PauseMenuUIHandler

--- Assets/Scripts/UI/PauseMenuUIHandler.cs
+++ Assets/Scripts/UI/PauseMenuUIHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuUIHandler : MonoBehaviour
 {
@@ -26,6 +27,6 @@ public class PauseMenuUIHandler : MonoBehaviour
 
     public void ReturnToMainMenu()
     {
-
+        SceneManager.LoadScene("TitleScreen");
     }
 }
