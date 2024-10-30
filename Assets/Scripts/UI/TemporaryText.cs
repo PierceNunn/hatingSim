f34ec9b657d4dbb2f70b0b2f685243a98d875126
PierceNunn <agentp5151@gmail.com>
2024-10-30 09:59:54 -0500

    created TemporaryText.cs

---
+++ Assets/Scripts/UI/TemporaryText.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TemporaryText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    void Start()
+    {
+        StartCoroutine(FadeTextToZeroAlpha(10f, text));
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, TextMeshProUGUI i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
+}
