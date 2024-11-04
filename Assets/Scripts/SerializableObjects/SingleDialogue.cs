4101ce9ffdba0ab224f47286eaa682a3d36d7af4
PierceNunn <agentp5151@gmail.com>
2024-11-04 10:03:36 -0600

    added cameraEffect field to SingleDialogue

--- Assets/Scripts/SerializableObjects/SingleDialogue.cs
+++ Assets/Scripts/SerializableObjects/SingleDialogue.cs
@@ -13,12 +13,15 @@ public class SingleDialogue
 {
     [SerializeField] private CharacterData _talkerData;
     [SerializeField] private string _portraitID;
-    [SerializeField] private UnityEvent[] _eventsToInvoke; //for calling events mid-conversation
     [TextArea(4, 10)] //set size of text field in editor
     [SerializeField] private string _sentences;
+    [SerializeField] private Enums.CameraEffects _cameraEffect;
+    [SerializeField] private UnityEvent[] _eventsToInvoke; //for calling events mid-conversation
+    
 
     public UnityEvent[] EventsToInvoke { get => _eventsToInvoke; set => _eventsToInvoke = value; }
     public string sentences { get => _sentences; set => _sentences = value; }
     public CharacterData TalkerData { get => _talkerData; set => _talkerData = value; }
     public string PortraitID { get => _portraitID; set => _portraitID = value; }
+    public Enums.CameraEffects CameraEffect { get => _cameraEffect; set => _cameraEffect = value; }
 }
