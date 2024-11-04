d39f42c9faebba62758296d43cab60a3f5e31989
PierceNunn <agentp5151@gmail.com>
2024-11-04 10:01:10 -0600

    created enum CameraEffects

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Enums : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum CameraEffects
     {
-        
-    }
+        none,
+        screenShake,
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
