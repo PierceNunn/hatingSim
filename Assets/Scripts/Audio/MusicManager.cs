556c95e866b1c195d479a81cc86b3cc88af0bf8f
kittynomad <59345740+kittynomad@users.noreply.github.com>
2024-11-12 11:46:19 -0600

    set up MusicManager script

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    [SerializeField] private AudioClip _morningMusic;
+    [SerializeField] private AudioClip _eveningMusic;
+    [SerializeField] private AudioSource _musicPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetMusic();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetMusic()
+    {
+        if(PlayerPrefs.GetInt("currentTime", 0) == 0)
+        {
+            _musicPlayer.clip = _morningMusic;
+        }
+        else
+        {
+            _musicPlayer.clip = _eveningMusic;
+        }
     }
 }
