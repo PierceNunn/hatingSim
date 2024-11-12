619930f403141af071e0f455be6a08188dbac658
kittynomad <59345740+kittynomad@users.noreply.github.com>
2024-11-12 12:55:05 -0600

    added variable in TimeAdvanceNode to advance time by more than 1

--- Assets/Scripts/Nodes/TimeAdvanceNode.cs
+++ Assets/Scripts/Nodes/TimeAdvanceNode.cs
@@ -5,11 +5,11 @@ using XNode;
 using UnityEngine.SceneManagement;
 
 public class TimeAdvanceNode : EndingNode {
-
+    [SerializeField] private int _advanceTimeAmt = 1;
     public override void OnCall()
     {
         base.OnCall();
-        TimeUIManager.AdvanceTime();
+        TimeUIManager.AdvanceTime(_advanceTimeAmt);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
