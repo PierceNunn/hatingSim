8cd283a6fc8b0018ee93a9db3941b5afd7e6b32b
PierceNunn <agentp5151@gmail.com>
2024-11-13 09:47:08 -0600

    added check in TileMapTeleport to ensure collision is with Player before activating

--- Assets/Scripts/Map Scripts/TileMapTeleport.cs
+++ Assets/Scripts/Map Scripts/TileMapTeleport.cs
@@ -22,7 +22,7 @@ public class TileMapTeleport : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(IsUsable())
+        if(collision.gameObject.CompareTag("Player") && IsUsable())
         {
             Teleport();
         }
