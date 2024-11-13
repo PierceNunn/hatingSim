4b4b537dd175df3d968959e77adaf1276889dd04
PierceNunn <agentp5151@gmail.com>
2024-11-13 09:53:19 -0600

    added logic for ItemDependantTileMapTeleport's IsUsable override

--- Assets/Scripts/Map Scripts/ItemDependantTileMapTeleport.cs
+++ Assets/Scripts/Map Scripts/ItemDependantTileMapTeleport.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class ItemDependantTileMapTeleport : TileMapTeleport
 {
-
+    [SerializeField] private ItemsForDay[] _daysWithRequiredItems;
+    public override bool IsUsable()
+    {
+        foreach(ItemsForDay day in _daysWithRequiredItems)
+        {
+            if(day.TargetDay == PlayerPrefs.GetInt("currentDay", 0) &&
+                day.TargetPhase == (Enums.DayPhases)PlayerPrefs.GetInt("currentTime", 0))
+            {
+                foreach(CollectibleItem item in day.TargetItems)
+                {
+                    if (!CollectibleItem.IsItemCollected(item))
+                        return false;
+                }
+            }
+        }
+        return base.IsUsable();
+    }
 }
