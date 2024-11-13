b75f4b8700a65490c8a67ff431c458447201895b
PierceNunn <agentp5151@gmail.com>
2024-11-13 09:56:13 -0600

    added comments to ItemDependantTileMapTeleport

--- Assets/Scripts/Map Scripts/ItemDependantTileMapTeleport.cs
+++ Assets/Scripts/Map Scripts/ItemDependantTileMapTeleport.cs
@@ -7,18 +7,23 @@ public class ItemDependantTileMapTeleport : TileMapTeleport
     [SerializeField] private ItemsForDay[] _daysWithRequiredItems;
     public override bool IsUsable()
     {
+        //iterate through each day listed in daysWithRequiredItems
         foreach(ItemsForDay day in _daysWithRequiredItems)
         {
+            //check if the day+time of array item match up with the current day+time
             if(day.TargetDay == PlayerPrefs.GetInt("currentDay", 0) &&
                 day.TargetPhase == (Enums.DayPhases)PlayerPrefs.GetInt("currentTime", 0))
             {
+                //if day+time lines up, iterate through all items listed for that day
                 foreach(CollectibleItem item in day.TargetItems)
                 {
+                    //if a required item hasn't been collected, teleporter isn't usable
                     if (!CollectibleItem.IsItemCollected(item))
                         return false;
                 }
             }
         }
+        //if false hasn't been called at this point the teleporter is usable
         return base.IsUsable();
     }
 }
