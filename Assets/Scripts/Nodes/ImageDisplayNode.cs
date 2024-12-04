d115c38879274fca0a5516f7b770ba160218848c
kittynomad <59345740+kittynomad@users.noreply.github.com>
2024-12-04 12:24:23 -0600

    created ImageDisplayNode for use in endings

---
+++ Assets/Scripts/Nodes/ImageDisplayNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class ImageDisplayNode : DialogueNode {
+	[SerializeField] private Sprite _imageToDisplay;
+
+    public Sprite ImageToDisplay { get => _imageToDisplay; set => _imageToDisplay = value; }
+
+    public override void DialoguePlayBehavior()
+    {
+
+    }
+}
