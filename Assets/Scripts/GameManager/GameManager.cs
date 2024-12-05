9bd8a06ccab99693ee3bbafe847f45a0bb69f17a
kittynomad <59345740+kittynomad@users.noreply.github.com>
2024-12-05 10:19:04 -0600

    GoldMaster

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public const double GAME_VERSION = 0.1001;
+    public const double GAME_VERSION = 0.2;
 
     public static GameManager instance;
 
